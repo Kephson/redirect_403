c6c84884d240a8c8b9f7deec49c2c87e98f9f6aa
Ephraim Härer <ephraim@ephespage.de>
2022-08-04 20:12:03 +0200

    removed global ext conf and moved to sites config

---
+++ Configuration/TCA/Overrides/sites.php
@@ -0,0 +1,43 @@
+<?php
+// Experimental example to add a new field to the site configuration
+
+// Configure a new simple required input field to site
+$GLOBALS['SiteConfiguration']['site_errorhandling']['columns']['protectedInfoUid'] = [
+    'label' => 'Page id with information about protection',
+    'description' => 'my desc 1',
+    'config' => [
+        'type' => 'input',
+        'eval' => 'required',
+        'renderType' => 'inputLink',
+        'fieldControl' => [
+            'linkPopup' => [
+                'options' => [
+                    'blindLinkOptions' => 'file,telephone,mail,spec,folder',
+                ]
+            ],
+        ],
+    ],
+];
+$GLOBALS['SiteConfiguration']['site_errorhandling']['columns']['loginPageUid'] = [
+    'label' => 'Page id of login page',
+    'description' => 'my desc 2',
+    'config' => [
+        'type' => 'input',
+        'eval' => 'required',
+        'renderType' => 'inputLink',
+        'fieldControl' => [
+            'linkPopup' => [
+                'options' => [
+                    'blindLinkOptions' => 'file,telephone,mail,spec,folder',
+                ]
+            ],
+        ],
+    ],
+];
+
+// And add it to showitem
+$GLOBALS['SiteConfiguration']['site_errorhandling']['types']['Page'] = str_replace(
+    'errorContentSource',
+    'errorContentSource, protectedInfoUid, loginPageUid',
+    $GLOBALS['SiteConfiguration']['site_errorhandling']['types']['Page']
+);
